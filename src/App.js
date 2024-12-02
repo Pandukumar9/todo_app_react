e5481e028c8dbfb20acbbc5a022c8bb9b9c24890
Pandukumar9 <pandukumar.gujja@gmail.com>
2024-12-02 17:43:39 +0530

    logic was added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [todolist, updateTodos] = useState([
+    {id:1 , task:"Angular"},
+    {id:2 , task:"React"},
+  ]);
+
+  let [todoInput , updateInput] = useState('');
+
+  let nextid =3;
+  function addTodos(){
+    if(todoInput === 0){
+      alert("Add some task");
+    }else{
+     let newtodos = [
+        ...todolist,
+        {
+          id: nextid++,
+          task : todoInput
+        }
+      ]
+      updateTodos(newtodos);
+      updateInput("");
+    }
+  }
+
+  function deleteTodo(id){
+    let updatedtods = todolist.filter(
+      (todo) => {
+        return todo.id !== id;
+      }
+    )
+    updateTodos(updatedtods);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5 w-50'>
+      <h1 className='text-center'>Todo app using in react</h1>
+      <div className='input-group'>
+        <input type='text'className='form-control' onChange={(e) => {
+          let task = e.target.value;
+          updateInput(task);
+        }} value={todoInput}/>
+        <button className='btn btn-primary' onClick={addTodos}>Add</button>
+      </div>
+      <ul className='list-group mt-4'>
+        {
+          todolist.map((todo) =>{
+            return (
+              <li className='list-group-item'> 
+                 <p>{ todo.task }</p>
+                 <button className='btn' onClick={() => {
+                  deleteTodo(todo.id)
+                 }}>❌</button>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
